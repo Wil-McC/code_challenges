4ad6be9cc38bb0c1c44a2ed2f08dafd13f4aba35
Wil McCauley <wmccauley724@gmail.com>
2021-06-23 11:22:40 -0600

    Add initial approach framework

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -1,5 +1,24 @@
 # @param {String[]} strs
 # @return {String}
-def longest_common_prefix(strs)
 
+# step through each element as long as all elements have the same letters
+# letters that match for all elements are stored in an acc
+# return acc
+
+# elements.all? do |elem|
+
+# end
+# assumption: max possible common prefix limited to length of shortest word
+
+strs = ["flower","flow","flight"]
+
+def longest_common_prefix(strs)
+  acc = ""
+  count = 0
+  letter = strs.first[count]
+  while count < strs.first.length
+
+  end
 end
+
+p longest_common_prefix(strs)
