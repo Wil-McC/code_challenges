b823c516d447b16c429279197dadeef796edbc92
Wil McCauley <wmccauley724@gmail.com>
2021-06-28 11:29:52 -0600

    Add more notes

--- 3sum.rb
+++ 3sum.rb
@@ -2,9 +2,16 @@
 # nums = [-1,0,1,2,-1,-4]
 # i + k + j == 0
 # i != j != k
+# 0 - x = -1? x == -1
+# add 2 together, see if there is an elem that cancels out value
+# while none equal another and sum != 0 and count < 3
+#
+# check each in isolation against remaining
+# while sum == 0 AND length < 3, add numbers
 
 nums = [-1,0,1] # ,2,-1,-4]
 
 def three_sum(nums)
+
 
 end
