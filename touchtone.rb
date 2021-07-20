b7274335fb13756bafa2a06bfecc896356623286
Wil McCauley <wmccauley724@gmail.com>
2021-07-20 11:08:45 -0600

    Add initial noteS

---
+++ touchtone.rb
@@ -0,0 +1,6 @@
+# return all possible combinations of symbolized letters
+# order does NOT matter
+
+# all numbers 2..9 are associated with a set of 3-4 letters
+
+# output is an ARRAY of STRINGS whose LENGTH matches the length of the input digit - gatekeep?
