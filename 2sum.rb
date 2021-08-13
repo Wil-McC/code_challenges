c4ead81bb1294f033c76461c437c904b1463005e
Wil McCauley <wmccauley724@gmail.com>
2021-08-13 15:20:10 -0600

    Add cleaning to make comparison more efficient

--- 2sum.rb
+++ 2sum.rb
@@ -5,8 +5,17 @@
 
 # assuming:
 #   - one good answer in array
-#   -
+#   - cannot repeat same element!
+
+# discount all elements greater or equal to target
 
 def two_sum(nums, target)
-  # insert magic here
+  # indices of nums that equal target - map?
+  clean = nums.select do |num|
+    num < target
+  end
+  
 end
+
+# expect [0, 1]
+p two_sum([2,7,11,15], 9)
