e6848eb64a928fb335af08281de4d8aa316e52d8
Wil McCauley <wmccauley724@gmail.com>
2021-08-13 12:18:19 -0600

    Clarify notes

---
+++ 2sum.rb
@@ -0,0 +1,12 @@
+# given an array of integers, return the INDICES of the 2 numbers hwich add to target number
+
+# IN - ( ARRAY of INT, target INT )
+# OUT - ARRAY of INDICES ***
+
+# assuming:
+#   - one good answer in array
+#   -
+
+def two_sum(nums, target)
+  # insert magic here
+end
