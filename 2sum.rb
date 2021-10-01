f35bced4150d457d6efb6ccd3203f24715fc7d75
Wil McCauley <wmccauley724@gmail.com>
2021-10-01 10:56:39 -0600

    Refactor cleaning method and add escape clause when only remaining values sum to target

--- 2sum.rb
+++ 2sum.rb
@@ -1,9 +1,12 @@
 # given an array of integers, return the INDICES of the 2 numbers hwich add to target number
 
+#
+
 # IN - ( ARRAY of INT, target INT )
 # OUT - ARRAY of INDICES ***
 
 # assuming:
+#   - ONLY TWO NUMBERS
 #   - one good answer in array
 #   - cannot repeat same element!
 
@@ -12,14 +15,22 @@
 def two_sum(nums, target)
   # make a hash to link
   acc = Hash.new
+  # build value - index linking hash to retain original indices
   nums.each_with_index do |item, index|
     acc[index] = item
   end
-  # if 2 sum to target, value of key added to accumulator
-  clean = acc.select do |k, v|
-    v < target
+  # clean out numbers >= target to reduce complexity
+  # clean = acc.select do |k, v|
+  #   v < target
+  # end
+  clean = nums.select do |num|
+    num < target
   end
-  require "pry"; binding.pry
+
+  if clean.sum == target
+    return 
+  # h.key(value) --> return key of value if present
+  c = 0
 end
 
 # expect [0, 1]
