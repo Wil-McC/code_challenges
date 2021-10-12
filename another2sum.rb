bd2022099dc3a37fe5aef4b8eed4a7dd59980a5f
Wil McCauley <wmccauley724@gmail.com>
2021-10-12 11:36:52 -0600

    Attempt with 2 accumulators to handle hash structure

---
+++ another2sum.rb
@@ -0,0 +1,31 @@
+# HASH BASED ATTEMPT
+
+# IN  - an ARRAY of INTEGERS, an INTEGER target
+# OUT - an ARRAY of INDEX POINTERS
+
+def two_sum(nums, target)
+  h = Hash.new
+  nums.each_with_index do |item, index|
+    h[index] = item
+  end
+
+  cleaned = h.select do |k, v|
+    v < target
+  end
+
+  if cleaned.length == 2
+    return cleaned.map do |k, v|
+       k
+    end
+  else
+    current_index = 0
+    proto =
+  end
+
+end
+
+numbers = [1, 12, 9, 2]
+t       = 3
+
+p two_sum(numbers, t)
+# expect [0, 3]
