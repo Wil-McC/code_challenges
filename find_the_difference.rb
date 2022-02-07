2ecf88f5f3fdfe4b9b8361714fd68836a24c6371
Wil McCauley <wmccauley724@gmail.com>
2022-02-07 11:25:16 -0700

    Mostly solve except for non-freq solutions

--- find_the_difference.rb
+++ find_the_difference.rb
@@ -1,26 +1,31 @@
 # given S and T, find the character that was added by T
 # find the character present in T that is missing in S
-# work linearly - return first non-matching character
+# T is a scrambled version of S - meaning only letter frequency will count
+# find the letter whose freq count is +1 compared with S
 
 def find_the_difference(s, t)
   return "invalid string" unless t.length - s.length == 1
 
-  baseline = s.split('')
-  salted   = t.split('')
-
-  i = 0
-  while i < salted.length
-    if baseline[i] != salted[i]
-      return salted[i]
-    elsif i == baseline.length
-      return salted[-1]
-    else
-      i += 1
-    end
+  s_hash = hash_digest(s)
+  t_hash = hash_digest(t)
+  require "pry"; binding.pry
+  t_hash.select do |k, v|
+    v - s_hash[k] == 1
+    return k
   end
 end
 
-string = 'a'
-test   = 'aa'
+def hash_digest(string)
+  char_array = string.split('')
+  output_hash = Hash.new(0)
+  char_array.each do |char|
+    output_hash[char] += 1
+  end
+  output_hash
+end
+
+string = 'abcd'
+test   = 'abcde'
+# expect 'e'
 
 p find_the_difference(string, test)
