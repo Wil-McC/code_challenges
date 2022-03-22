64757d2343151fad09681853654575757ddb8145
Wil McCauley <wmccauley724@gmail.com>
2022-03-22 10:59:05 -0600

    Finish basic outline

---
+++ partition_labels.rb
@@ -0,0 +1,9 @@
+# partition string into max number of segments where chars only appear in one segment
+# where : final_segments.concat() == s
+
+# IN : String of chars EX: 'abc'
+# OUT: Array of partition length integers EX: [1, 1, 1]
+
+def partition_labels(s)
+
+end
