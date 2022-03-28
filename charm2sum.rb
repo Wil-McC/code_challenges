8500c500df182bce4bdbac56b8667cfa65cd6da8
Wil McCauley <wmccauley724@gmail.com>
2022-03-28 12:02:24 -0600

    Start back up again

--- charm2sum.rb
+++ charm2sum.rb
@@ -1,16 +1,21 @@
+# OUT: *indices* of 2 elements that sum to target
+
 def two_sum(nums, target)
   # create hash to link original index position with values
   cleaned = Hash.new
 
   nums.each_with_index do |item, index|
-    cleaned[index] = item
+    if item < target
+      cleaned[index] = item
+    end
   end
-
+  require "pry"; binding.pry
   # cleaned.key(val)
   acc = Array.new
-  
+
 end
 
+# expect [1, 2] OR [2, 1]
 numbers = [3, 2, 4]
 t       = 6
 
