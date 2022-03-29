c797398db975878488bb4957b89ba200444c1fe3
Wil McCauley <wmccauley724@gmail.com>
2022-03-29 11:54:29 -0600

    Solve for positive integers, breaks on target zero

--- charm2sum.rb
+++ charm2sum.rb
@@ -3,7 +3,6 @@
 def two_sum(nums, target)
   # create hash to link original index position with values
   cleaned = Hash.new
-
   nums.each_with_index do |item, index|
     if item < target
       cleaned[index] = item
@@ -18,11 +17,18 @@ def two_sum(nums, target)
     # get needed pair to hit target (target - current_val)
     # check for that value in hash vals
     # if that val, add current_val key (idx) and pair_val key (idx) to output array
+    needed_pair = target - v
+    if cleaned.key(needed_pair) && cleaned.key(needed_pair) != k
+      acc << k
+      acc << cleaned.key(needed_pair)
+      break
+    end
   end
+  return acc
 end
 
-# expect [1, 2] OR [2, 1]
-numbers = [3, 2, 4]
-t       = 6
+# expect [0, 3]
+numbers = [0,4,3,0]
+t       = 0
 
 p two_sum(numbers, t)
