00f04419557040de8a2f562960772dcf6933550d
Wil McCauley <wmccauley724@gmail.com>
2022-04-11 10:51:02 -0600

    Initial setup and approach

---
+++ shift_2d_grid.rb
@@ -0,0 +1,15 @@
+# subarrays have same number of elements
+# shift grid k times
+
+# a.unshift(elem)
+# a.pop -> returns last element
+
+# IN : 2D ARRAY grid, INT k
+# OUT: 2D ARRAY shifted k times
+
+
+def shift_grid(grid, k)
+  # k times do: pop last and unshift to first
+  # pop first and unshift to next
+  # repeat until second-to-last array
+end
