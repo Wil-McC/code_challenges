71a12cd935c57ad15341a26f0e7f1d1a94ccca20
Wil McCauley <wmccauley724@gmail.com>
2022-05-16 12:35:22 -0600

    Simplify approach and begin if cases

--- shift_2d_grid.rb
+++ shift_2d_grid.rb
@@ -9,7 +9,32 @@
 
 
 def shift_grid(grid, k)
-  # k times do: pop last and unshift to first
-  # pop first and unshift to next
-  # repeat until second-to-last array
+
+  grid_size = grid.length - 1
+  counter = k
+  current_index = 0
+
+  while counter > 0 do
+
+    if counter == k
+      # take from last and move to first
+      grid[current_index].unshift(grid[current_index - 1].pop)
+      current_index += 1
+      counter -= 1
+    elsif current_index == grid_size
+      # take from first and move to next
+
+
+  end
+  return grid
 end
+# if i == grid_size, i = 0
+# grid[0..-2].each { |sub_arr| sub_arr.unshift(current_val); current_val = sub_arr.pop }
+# while k > 0
+# pop off last val
+# unshift it onto first arr
+# pop off last val in first arr
+# unshift it onto next arr
+# ......
+# when current arr == last arr
+# START OVER
