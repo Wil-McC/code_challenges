1400158c98db18e21014387a46b1feecfe671274
Wil McCauley <wmccauley724@gmail.com>
2022-05-24 10:37:06 -0600

    Complete a messy but fairly readable solution

--- shift_2d_grid.rb
+++ shift_2d_grid.rb
@@ -1,26 +1,34 @@
 # subarrays have same number of elements
 # shift grid k times
 
-# a.unshift(elem)
-# a.pop -> returns last element
-
 # IN : 2D ARRAY grid, INT k
 # OUT: 2D ARRAY shifted k times
 
-
 def shift_grid(grid, k)
+  grid_size = grid.length
   row_size = 0
-  if grid.all? { |arr| arr.length = grid[0].length }
-    row_size = arr.length
+  if grid.all? { |arr| arr.length == grid[0].length }
+    row_size = grid[0].length
   else
     return 'Grid rows uneven'
   end
 
+  flat_grid = grid.flatten
 
-  return grid
+  k.times do
+    flat_grid.rotate!(-1)
+  end
+
+  new_grid = Array.new
+
+  grid_size.times do
+    new_grid << flat_grid.pop(row_size)
+  end
+
+  return new_grid.reverse
 end
 
-foo = [[1,2,3], [4,5,6], [7,8,9]]
-x = 1
+foo = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]]
+x = 4
 
 p shift_grid(foo, x)
